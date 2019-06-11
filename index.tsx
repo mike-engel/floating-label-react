70f0f409c01c14aba8622a291369f4e0c6b87000
Mike Engel <mike@mike-engel.com>
2019-06-11 11:13:09 -0600

    chore: Update dependencies

--- index.tsx
+++ index.tsx
@@ -5,18 +5,14 @@ interface Props extends HTMLProps<HTMLInputElement | HTMLTextAreaElement> {
   component?: "input" | "textarea";
 }
 
-const FloatingLabel = ({ component = "input", placeholder, className = "", ...rest }: Props) => {
-  console.log(rest, className);
-
-  return (
-    <label
-      className={`floating-label ${className} ${!!rest.value ? "floating" : ""}`}
-      htmlFor={rest.id}
-    >
-      {React.createElement(component, rest)}
-      <span>{placeholder}</span>
-    </label>
-  );
-};
+const FloatingLabel = ({ component = "input", placeholder, className = "", ...rest }: Props) => (
+  <label
+    className={`floating-label ${className} ${!!rest.value ? "floating" : ""}`}
+    htmlFor={rest.id}
+  >
+    {React.createElement(component, rest)}
+    <span>{placeholder}</span>
+  </label>
+);
 
 export default FloatingLabel;
